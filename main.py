66a8bf1991d4a416a56ca8b67654330d22c533d5
赵宇 <zhaoyu@zhaoyudeMBP.lan>
2021-07-13 18:33:09 +0200

    add complete project

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from satpy.composites import GenericCompositor
 from satpy.writers import to_image
 from pyresample import create_area_def
 import matplotlib.pyplot as plt
+from pyproj import Transformer
 if __name__=='__main__':
     print(available_readers())
     path_to_data = '/Users/zhaoyu/PycharmProjects/LowResSatellitesService/data/VNPSDR'
@@ -13,15 +14,20 @@ if __name__=='__main__':
     scn = Scene(filenames=files)
     print(scn.available_dataset_names())
     print(scn.available_composite_names())
-    scn.load(['I01', 'I02', 'I03', 'I04', 'I05'])
+    scn.load(['I04', 'I05'])
+    transformer = Transformer.from_crs(4326, 32610)
+    bot_left = transformer.transform(36.7, -119.8)
+    top_right = transformer.transform(37.7, -118.8)
 
-    my_area = create_area_def('my_area', projection='epsg:4326', height=scn['I04'].y.size, width=scn['I04'].x.size)
+    my_area = create_area_def('my_area', projection='epsg:32610', resolution=scn['I04'].resolution, area_extent=[bot_left[0], bot_left[1], top_right[0], top_right[1]])
+
+    new_scn = scn.resample(destination=my_area)
     # compositor = GenericCompositor("overview")
     # composite = compositor([scn['I01'],scn['I02'],scn['I03'],scn['I04'],scn['I05']])
     # # lon=scn['i_lon'].values
     # lat=scn['i_lat'].values
     # area = create_area_def(area_id="area", projection='WGS84', lon=scn['i_lon'].values, lat=scn['i_lat'].values)
-    new_scn = scn.resample(destination=my_area)
+    # scn_crop =
     # plt.figure()
     # new_scn['I04'].plot.imshow()
     # plt.show()
--- main_s3.py
+++ main_s3.py
@@ -1,87 +1,10 @@
-import oauth as oauth
-from satpy.scene import Scene
-from glob import glob
-from satpy import find_files_and_readers
-from satpy import available_readers
-from satpy.composites import GenericCompositor
-from satpy.writers import to_image
-from pyresample import create_area_def
-import matplotlib.pyplot as plt
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
-from sentinelhub import SHConfig
-from sentinelhub import MimeType, CRS, BBox, SentinelHubRequest, SentinelHubDownloadClient, \
-    DataCollection, bbox_to_dimensions, DownloadRequest
+from Preprocessing.PreprocessingService import PreprocessingService
+from LowResSatellitesService.LowResSatellitesService import LowResSatellitesService
 
 if __name__=='__main__':
-    # Your client credentials
-    client_id = '4456b422-fe16-4f5c-90b1-5a9583020e10'
-    client_secret = '@(KXCxP34*e:nFh@/azmkH~*cEnA%KwT-qm{!q!-'
-    # Create a session
-    # client = BackendApplicationClient(client_id=client_id)
-    # oauth = OAuth2Session(client=client)
-    # # Get token for the session
-    # token = oauth.fetch_token(token_url='https://services.sentinel-hub.com/oauth/token',
-    #                           client_id=client_id, client_secret=client_secret)
-    # # All requests using this session will have an access token automatically added
-    # resp = oauth.get("https://services.sentinel-hub.com/oauth/tokeninfo")
-    # print(resp.content)
-    # print(token)
-    config = SHConfig()
-
-    if config.sh_client_id == '' or config.sh_client_secret == '':
-        print("Warning! To use Sentinel Hub Process API, please provide the credentials (client ID and client secret).")
-    else:
-        print('Using Id:{}'.format(config.sh_client_id))
-
-    betsiboka_coords_wgs84 = [-118.8, 36.7, -119.8, 37.7]
-
-    resolution = 1000
-    betsiboka_bbox = BBox(bbox=betsiboka_coords_wgs84, crs=CRS.WGS84)
-    betsiboka_size = bbox_to_dimensions(betsiboka_bbox, resolution=resolution)
-
-    print(f'Image shape at {resolution} m resolution: {betsiboka_size} pixels')
-
-    evalscript_true_color = """
-        //VERSION=3
-
-        function setup() {
-            return {
-                input: [{
-                    bands: ["S7"]
-                }],
-                output: {
-                    bands: 1
-                }
-            };
-        }
-
-        function evaluatePixel(sample) {
-            return [0.002*sample.S7];
-        }
-    """
-
-    request_true_color = SentinelHubRequest(
-        evalscript=evalscript_true_color,
-        input_data=[
-            SentinelHubRequest.input_data(
-                data_collection=DataCollection.SENTINEL3_SLSTR,
-                time_interval=('2020-09-07', '2020-09-08'),
-            )
-        ],
-        responses=[
-            SentinelHubRequest.output_response('default', MimeType.TIFF)
-        ],
-        bbox=betsiboka_bbox,
-        size=betsiboka_size,
-        config=config
-    )
-    true_color_imgs = request_true_color.get_data()
-    image = true_color_imgs[0]
-    print(f'Image type: {image.dtype}')
-
-    # plot function
-    # factor 1/255 to scale between 0-1
-    # factor 3.5 to increase brightness
-    plt.imshow(image, vmin=image.min(), vmax=image.max(), cmap='Greys')
-    plt.show()
+    location = 'kamloop'
+    lowres = LowResSatellitesService()
+    # lowres.fetch_imagery_from_sentinel_hub(location, ["S3"])
+    # lowres.fetch_imagery_from_sentinel_hub_custom(location, ["S3"])
+    preprocessing = PreprocessingService()
+    preprocessing.get_thresholded_image(location)
